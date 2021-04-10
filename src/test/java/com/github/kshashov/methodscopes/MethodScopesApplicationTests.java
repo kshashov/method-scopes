85f58b4c9da72f0b0a1bcc7b2df73f1fbf5e8279
Kirill Shashov <envoy93@gmail.com>
2021-04-10 15:17:33 +0400

    Init project

---
+++ src/test/java/com/github/kshashov/methodscopes/MethodScopesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kshashov.methodscopes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MethodScopesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
